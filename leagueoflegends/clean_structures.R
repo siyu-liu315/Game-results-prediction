4dc714958e33cba7a1dc3168074d7cd3646e3fdc
Qiuhao <qchengyo@bu.edu>
2019-10-09 10:32:36 -0400

    won't push

--- leagueoflegends/clean_structures.R
+++ leagueoflegends/clean_structures.R
@@ -16,7 +16,7 @@ class(bhome$Type)
 bhome$Type <- as.vector(bhome$Type)
 bhome$Type <- str_replace(bhome$Type, "FOUNTAIN_TURRET", "BASE_TURRET")
 ### 利用下面的数据确定数据对错
-tem <- structures %>% filter(Address == "055b17da8456fdc8") %>% view()
+tem <- structures %>% filter(Type == "NEXUS_TURRET") %>% view()
 
 bhome %>% mutate(top_outer = (Lane == "TOP_LANE" & Type == "OUTER_TURRET"),
                  top_inner = (Lane == "TOP_LANE" & Type == "INNER_TURRET"),
@@ -27,7 +27,8 @@ bhome %>% mutate(top_outer = (Lane == "TOP_LANE" & Type == "OUTER_TURRET"),
                  mid_inhibitor = (Lane == "MID_LANE" & Type == "INHIBITOR")) %>% 
           mutate(bot_outer = (Lane == "BOT_LANE" & Type == "OUTER_TURRET"),
                  bot_inner = (Lane == "BOT_LANE" & Type == "INNER_TURRET"),
-                 bot_base = (Lane == "BOT_LANE" & Type == "BASE_TURRET")) -> bhome
+                 bot_base = (Lane == "BOT_LANE" & Type == "BASE_TURRET"),
+                 mid_nexus = ((Lane == "MID_LANE" & Type == "NEXUS_TURRET"))) -> bhome
 ### replace Ture or False to 0/1
 bhome %>% select(-c(Address, Team, Lane, Type, Time)) -> tower
 infor <- bhome %>% select(c(Address, Team, Lane, Type, Time)) 
@@ -35,11 +36,24 @@ tower <- ifelse(tower == "TRUE", 1, 0)
 b_tower <- cbind(infor, tower)
 ### 检测是否每一分钟只推掉一座塔
 rowSums(tower) %>% unique()
-### 把team inhibit 和time 转换成0
-b_tower$Team <- str_replace(b_tower$Team, "bInhibs", "0")
-view(b_tower$Time)
-b_tower$Time <- str_replace(b_tower$Time, "NA", "0")
-b_tower %>% filter(Time == "NA")
+### 把NA去除,无效
+b_tower <- na.omit(b_tower)
+write_excel_csv(b_tower,file = "temp1")
+
+### teams
+teamname <- unique(b_tower$Address) %>% as.matrix()
+teamname <- teamname[rep(seq_len(nrow(teamname)), each=60),]
+teamname$Time <- teamname$Time
+for (i in teamname) {
+  
+  
+}
+
+
+
+
+
+
 
 
 
