92613391ee197ad2693972507d76a910f9eaa9fc
Qiuhao <qchengyo@bu.edu>
2019-10-14 17:46:38 -0400

    add forest tree

--- Ridge and Lasso .R
+++ Ridge and Lasso .R
@@ -1,18 +1,24 @@
+install.packages(c("rpart.plot", "rpart"))
+install.packages(c("randomForest", "gbm"))
 library(ggplot2) 
 library(ggthemes) 
 library(glmnet)
 library(tidyverse)
 library(ggthemes)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(gbm)
 library(glmnet) # this is the library that allows us to run penalized regressions
 theme_set(theme_bw())
 
-final_30 <- final %>% filter(min == 12)
-view(final_30)
+final_20 <- final %>% filter(min == 20)
+view(final_20)
 
 set.seed(10086)
-final_30$train <- sample(c(0, 1), nrow(final_30), replace = TRUE, prob = c(.3, .7))
-test <- final_30 %>% filter(train == 0)
-train <- final_30 %>% filter(train == 1)
+final_20$train <- sample(c(0, 1), nrow(final_20), replace = TRUE, prob = c(.3, .7))
+test <- final_20 %>% filter(train == 0)
+train <- final_20 %>% filter(train == 1)
 
 
 
@@ -64,7 +70,46 @@ Lasso_lambda_min_mse_test <- Lasso_mse_test[which.min(Lasso_mse_test)] %>% print
 
 coef(Lasso)
 
+### Random Forest 
+f1 <- as.formula(bResult ~ golddiffADC + golddiffJungle + golddiffMiddle 
+                 + golddiffSupport + golddiffTop + top_outer_cum + top_inner_cum
+                 + top_base_cum + mid_outer_cum + mid_inner_cum + mid_base_cum
+                 + mid_inhibitor_cum + bot_outer_cum + bot_inner_cum + bot_base_cum
+                 + nexus_turret_cum + killer_cum + air_dragon_cum + earth_dragon_cum
+                 + fire_dragon_cum + water_dragon_cum + dragon_cum + baron_cum
+                 + elder_dragon_cum + rift_herald_cum)
+### Train random forest 
+rf_train <- randomForest(f1,
+                       train,
+                       ntree=100,
+                       do.trace=T)
+varImpPlot(rf_train)
 
+rf_y_train_hat <- predict(rf_train, x_train)
 
+rf_mse_train <- mean((rf_y_train_hat - y_train) ^ 2)
+### MSE for Train Data
+print(rf_mse_train)
+
+## Random Forest for Test
+rf_test <- randomForest(f1,
+                         test,
+                         ntree=100,
+                         do.trace= T)
+rf_y_test_hat <- predict(rf_test, x_test)
+
+rf_mse_test <- mean((rf_y_test_hat - y_test) ^ 2)
+### MSE for Train Data
+print(rf_mse_test)
+
+### Tree but useless 
+fit.tree <- rpart(f1,
+                  train,
+                  control = rpart.control(cp = 0.1))
+par(xpd = TRUE)
+plot(fit.tree, compress=TRUE)
+text(fit.tree, use.n=TRUE)
+
+rpart.plot(fit.tree)
 
 
