4edf2c57367c404b05ba220106b510c3be9300e2
Tyler-MSBA <53415112+Tyler-MSBA@users.noreply.github.com>
2019-10-15 16:50:24 -0400

    ok

--- loop_for_random_forests.R
+++ loop_for_random_forests.R
@@ -23,23 +23,21 @@ f1 <- as.formula(bResult ~ golddiffADC + golddiffJungle + golddiffMiddle
 ##
 final <- read.csv('final.csv')
 
+final$bResult <- as.factor(final$bResult)
+
 lst <- list()
 
 ### train_set spli
 
-<<<<<<< HEAD
 
-for (i in 1:60) {
-=======
 for (i in 1:10){
->>>>>>> 0060b4a5559efde530a3bd8fa6cc98aaca82a3e9
   
   df <- final %>% filter(min == i)
   
   df$train <- sample(c(0, 1), nrow(df), replace = TRUE, prob = c(.3, .7))
   
-  test <- df%>% filter(train == 0)
-  train <-df%>% filter(train == 1)
+  test <- df %>% filter(train == 0)
+  train <-df %>% filter(train == 1)
   
   x_train <- train %>% select(-bResult, -train, -matchname)
   y_train <- train %>% select(bResult)
@@ -47,40 +45,57 @@ for (i in 1:10){
   x_test <- test %>% select(-bResult, -train, -matchname)
   y_test <- test %>% select(bResult)
   
+  
   rf_train <- randomForest(f1,
                           train,
                           ntree=5,
                           do.trace= T)
   
-  rf_y_test_hat <- predict(rf_train, x_test)
+  
   rf_y_train_hat <- predict(rf_train, x_train)
+  rf_y_test_hat <- predict(rf_train, x_test)
   
   
-  lst[[i]] <- cbind.data.frame(rf_y_test_hat, y_test) %>% 
-    mutate(correct_prediction = 
-           rf_y_test_hat == y_test) %>% 
-    mutate(error_rate = 1 - (length(correct_prediction[correct_prediction == TRUE])) / length(correct_prediction))
+  rf_error_dataset_train <- cbind.data.frame(rf_y_train_hat, y_train)
+  rf_error_dataset_train <- rf_error_dataset_train %>%  
+                               mutate(correct_prediction = 
+                                   rf_error_dataset_train$rf_y_train_hat == rf_error_dataset_train$bResult)
+  
+  
+  rf_error_dataset_test <- cbind.data.frame(rf_y_test_hat, y_test)
+  rf_error_dataset_test <- rf_error_dataset_test %>%  
+                              mutate(correct_prediction = 
+                                   rf_error_dataset_test$rf_y_test_hat == rf_error_dataset_test$bResult)
+  
+  
+  rf_error_rate_train <- 1 - (length(rf_error_dataset_train[rf_error_dataset_train == TRUE]) / length(rf_error_dataset_train$correct_prediction))
+  rf_error_rate_test <- 1 - (length(rf_error_dataset_test[rf_error_dataset_test == TRUE]) / length(rf_error_dataset_test$correct_prediction))
+  
+  lst[[i]] <- cbind.data.frame(i, rf_error_rate_train, rf_error_rate_test)
     
 } 
-  
-  
-  
-  
-<<<<<<< HEAD
-} 
 
-=======
+View(lst)
+  
+
+
+
+
+lst[[i]] <- cbind.data.frame(rf_y_test_hat, y_test) %>% 
+  mutate(correct_prediction = 
+           rf_y_test_hat == y_test) %>% 
+  mutate(error_rate = 1 - (length(correct_prediction[correct_prediction == TRUE])) / length(correct_prediction))
+
   
   rf_error_dataset_test <- rf_error_dataset_test %>%  
     mutate(correct_prediction = 
              rf_error_dataset_test$rf_y_test_hat == rf_error_dataset_test$`test_tree$X15.bResult`)
   
-  x
+  
   rf_error_rate_train <- 1 - (length(rf_error_dataset_train[rf_error_dataset_train == TRUE]) / length(rf_error_dataset_train$correct_prediction))
   rf_error_rate_test <- 1 - (length(rf_error_dataset_test[rf_error_dataset_test == TRUE]) / length(rf_error_dataset_test$correct_prediction))
   
   
->>>>>>> 0060b4a5559efde530a3bd8fa6cc98aaca82a3e9
    
   
  
