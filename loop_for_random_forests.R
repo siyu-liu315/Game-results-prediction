4570952e971dd949f5659dd5492a0fbd6836254e
siyu-liu315 <53445006+siyu-liu315@users.noreply.github.com>
2019-10-16 03:41:56 +0800

    update loop

--- loop_for_random_forests.R
+++ loop_for_random_forests.R
@@ -1,1 +1,67 @@
-##
+
+
+library(ggplot2) 
+library(ggthemes) 
+library(glmnet)
+library(tidyverse)
+library(ggthemes)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(gbm)
+library(glmnet) # this is the library that allows us to run penalized regressions
+theme_set(theme_bw())
+
+##### Random Forest 
+f1 <- as.formula(bResult ~ golddiffADC + golddiffJungle + golddiffMiddle 
+                 + golddiffSupport + golddiffTop + top_outer_accum + top_inner_accum
+                 + top_base_accum + mid_outer_accum + mid_inner_accum + mid_base_accum
+                 + mid_inhibitor_accum + bot_outer_accum + bot_inner_accum + bot_base_accum
+                 + nexus_turret_accum + killer_accum + air_dragon_accum + earth_dragon_accum
+                 + fire_dragon_accum + water_dragon_accum + dragon_accum + baron_accum
+                 + elder_dragon_accum + rift_herald_accum)
+
+### train_set split
+
+mse_tibble <-tibble(min = "",
+                    mse_train = "",
+                    mse_test = "")
+
+for (i in 1:60){
+  df <- final %>% filter(min == i)
+  df$train <- sample(c(0, 1), nrow(df), replace = TRUE, prob = c(.3, .7))
+  test <- df%>% filter(train == 0)
+  train <-df%>% filter(train == 1)
+  
+  x_train <- train %>% select(-bResult, -train, -matchname)
+  y_train <- train %>% select(bResult)
+  x_test <- test %>% select(-bResult, -train, -matchname)
+  y_test <- test %>% select(bResult)
+  
+  ### Train random forest 
+  rf_train <- randomForest(f1,
+                           train,
+                           ntree=5,
+                           do.trace=T)
+  varImpPlot(rf_train)
+  rf_y_train_hat <- predict(rf_train, x_train)
+  rf_mse_train <- colMeans((rf_y_train_hat - y_train) ^ 2)
+  
+  rf_y_test_hat <- predict(rf_train, x_test)
+  rf_mse_test <- colMeans((rf_y_test_hat - y_test) ^ 2)
+  
+  ### MSE for Train Data
+  mse_tibble <-  add_row(mse_tibble,min = i,mse_train = rf_mse_train, mse_test = rf_mse_test)
+  }
+
+
+
+### Tree but useless 
+fit.tree <- rpart(f1,
+                  train,
+                  control = rpart.control(cp = 0.1))
+par(xpd = TRUE)
+plot(fit.tree, compress=TRUE)
+text(fit.tree, use.n=TRUE)
+
+rpart.plot(fit.tree)
